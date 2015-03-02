1d2faf0ec8aeffeab5f73fa37d7301699196a47a
Roger <rogerximenez@gmail.com>
2015-03-02 00:11:30 -0500

    add service and routes to the final product

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('NearMeApp', ['leaflet-directive']);
+var app = angular.module('NearMeApp', ['leaflet-directive', 'ngRoute'])
+	.config(function($routeProvider){
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainController',
+				redirectTo: '/',
+			})
+
+			.when('/about', {
+				templateUrl: 'views/about.html',
+				controller: 'AboutController'
+			});
+	});
